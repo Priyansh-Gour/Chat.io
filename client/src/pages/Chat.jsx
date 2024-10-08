01782790b4361946c2d9f84be4ddbf11d721471a
Priyansh-Gour <priyanshgour817@gmail.com>
2024-10-08 22:08:17 +0530

    working on chat component

--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -1,10 +1,66 @@
-import React from 'react'
-import AppLayout from '../components/layout/AppLayout'
+import React, { Fragment, useRef } from "react";
+import AppLayout from "../components/layout/AppLayout";
+import { IconButton, Stack } from "@mui/material";
+import { grayColor, orange } from "../constants/color";
+import { AttachFile as AttachFileIcon, Send as SendIcon} from "@mui/icons-material";
+import { InputBox } from "../components/Styles/StyledComponent";
+import FileMenu from "../components/dialogs/FileMenu";
 
 const Chat = () => {
+  const containerRef = useRef(null);
   return (
-    <div>Chat</div>
-  )
-}
+    <Fragment>
+      <Stack
+        ref={containerRef}
+        boxSizing={"border-box"}
+        padding={"1rem"}
+        spacing={"1rem"}
+        bgcolor={grayColor}
+        height={"90%"}
+        sx={{
+          overflowX:"hidden",
+          overflowY:"auto"
+        }}
+      >
+        {/* messages */}
+      </Stack>
 
-export default AppLayout()(Chat)
+      <form style={{
+        height:"10%"
+      }}
+      >
+        <Stack direction={"row"} height={"100%"} padding={"1rem"} alignItems={"center"} position={"relative"}>
+
+          <IconButton sx={{
+            position:"absolute",
+            left:"1.5rem",
+            rotate:"10deg"
+          }}>
+            <AttachFileIcon/>
+          </IconButton>
+
+          <InputBox placeholder="Type Message here..."/>
+
+          <IconButton type="submit" sx={{
+            rotate:"-10deg",
+            backgroundColor:orange,
+            color:"white",
+            marginLeft:"1rem",
+            padding:"0.5rem",
+            "&:hover":{
+              bgcolor:"error.dark"
+            }
+          }}>
+            <SendIcon/>
+          </IconButton>
+        </Stack>
+
+      </form>
+
+      <FileMenu />
+
+    </Fragment>
+  );
+};
+
+export default AppLayout()(Chat);
