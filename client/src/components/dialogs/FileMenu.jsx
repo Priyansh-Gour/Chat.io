8deeaad5542b30689b2b2dd09a7c051f59e4cb47
Priyansh-Gour <priyanshgour817@gmail.com>
2024-10-08 22:08:36 +0530

    Create FileMenu.jsx

---
+++ client/src/components/dialogs/FileMenu.jsx
@@ -0,0 +1,16 @@
+import { Menu } from '@mui/material'
+import React from 'react'
+
+const FileMenu = ({anchor}) => {
+  return (
+    <Menu anchorEl={anchor}>
+        <div style={{
+            width:"10rem",
+        }}>
+            lorem impsum 
+        </div>
+    </Menu>
+  )
+}
+
+export default FileMenu
