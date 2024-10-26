8dc256c454de38894cb7f90c66033e81c81721a8
Priyansh-Gour <priyanshgour817@gmail.com>
2024-10-26 18:49:46 +0530

    error handler

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,51 +1,46 @@
-import {User} from '../models/user.js'
-import { sendToken } from '../utils/features.js';
+import { User } from "../models/user.js";
+import { sendToken } from "../utils/features.js";
 import { compare } from "bcrypt";
-
+import { TryCatch } from "../middlewares/error.js";
 
 //create a new user and save it to database and save token in cookies
 const newUser = async (req, res) => {
+  const { name, username, password, bio } = req.body;
+  // console.log(req.body)
 
-    const { name, username, password, bio } = req.body;
-    // console.log(req.body)
+  const avatar = {
+    public_id: "chamanid",
+    url: "chamanurl",
+  };
 
-    const avatar = {
-        public_id: "chamanid",
-        url: "chamanurl"
-    }
+  const user = await User.create({
+    name,
+    username,
+    password,
+    avatar,
+    bio,
+  });
 
-    const user = await User.create({
-        name,
-        username,
-        password,
-        avatar,
-        bio
-    });
+  sendToken(res, user, 201, "User created successfully");
+  // res.status(201).json({message: "User created successfully"});
+};
 
-    sendToken(res,user,201,"User created successfully");
-    // res.status(201).json({message: "User created successfully"});
-}
-
-const login = async(req, res) => {
+const login = TryCatch(async (req, res, next) => {
     const { username, password } = req.body;
 
     const user = await User.findOne({ username }).select("+password");
 
-    if(!user){
-        return res.status(401).json({message: "User not found"});
-    }
+    if (!user) return next(new Error("User not found"));
 
     const isMatch = await compare(password, user.password);
 
-    if(!isMatch){
-        return res.status(401).json({message: "Invalid password"});
-    }
+    if (!isMatch) return next(new Error("Wrong password"));
 
-    sendToken(res,user,200,`Logged in successfully as ${user.name}`);
-}
+    sendToken(res, user, 200, `Logged in successfully as ${user.name}`);
+});
 
-const getMyProfile = async(req, res) => {
-    // return await User.findById(req.user._id);
-}
+const getMyProfile = async (req, res) => {
+  // return await User.findById(req.user._id);
+};
 
-export {login,newUser,getMyProfile};
+export { login, newUser, getMyProfile };
--- server/middlewares/error.js
+++ server/middlewares/error.js
@@ -1,4 +1,18 @@
 const errorMiddleware = (err, req, res, next) => {
-    
+    err.message = err.message || "Internal Server Error";
+    err.statusCode = err.statusCode || 500;
+
+    return res.status(err.statusCode).json({
+        success: false,
+        message: err.message,
+    });
 }
-export {errorMiddleware} first
+
+const TryCatch = (fn) => async(req, res, next) => {
+    try {
+        await fn(req, res, next);
+    } catch (err) {
+        next(err);
+    }
+}
+export {errorMiddleware, TryCatch};
