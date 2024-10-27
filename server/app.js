71c1142765e49346e1870bd1ec3a037554169cf0
Priyansh-Gour <priyanshgour817@gmail.com>
2024-10-27 17:14:45 +0530

    seeds

--- server/app.js
+++ server/app.js
@@ -6,6 +6,7 @@ import cookieParser from "cookie-parser";
 
 import userRoute from './routes/user.routes.js';
 import chatRoute from './routes/chat.routes.js';
+import { createUser } from "./seeders/user.js";
 
 dotenv.config({
     path:"./.env"
